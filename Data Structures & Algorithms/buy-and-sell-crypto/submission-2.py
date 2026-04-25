f291865557a5f9b9f4144fb32cd78bdc2e6c7ea3
Jordan <118320165+jordanjasonclifford@users.noreply.github.com>
2026-04-24 22:22:21 -0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+
+# need at least 2 prices to make a buy + sell
+        if len(prices) < 2:
+            return 0
+
+        # index of the lowest price seen so far (best buy day)
+        low = 0
+
+        # track the maximum profit we can get
+        profit = 0
+
+        # iterate through each day
+        for i in range(len(prices)):
+
+            # if current price is lower than our lowest so far,
+            # update the buy day
+            if prices[low] > prices[i]:
+                low = i
+
+            # calculate profit if we sell today
+            # (current price - lowest price so far)
+            if profit < prices[i] - prices[low]:
+                profit = prices[i] - prices[low]
+
+        return profit
+
+        
