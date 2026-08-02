b2b8e8e6f0f9730ea8cdd922f0ec823bfbab998f
Jordan <118320165+jordanjasonclifford@users.noreply.github.com>
2026-08-01 21:14:14 -0700

    Add: products-of-array-discluding-self - submission-7

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-7.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        # Initialize the result array with 1s.
+        # Each position will eventually contain:
+        # product of everything to the left * product of everything to the right
+        res = [1] * len(nums)
+
+        # Running product of all numbers to the left
+        prefix = 1
+
+        # First pass: store the product of everything to the left of each index
+        for i, n in enumerate(nums):
+            res[i] = prefix
+            prefix *= n
+
+        # Running product of all numbers to the right
+        postfix = 1
+
+        # Second pass: multiply each position by the product
+        # of everything to its right
+        for i in range(len(nums) -1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
